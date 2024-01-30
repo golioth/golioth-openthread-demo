603f505a281ed3ee237a078431334813aa2d4dbb
Marko Puric <marko@golioth.io>
2024-01-30 13:03:30 +0100

    app_work: remove ostentus and battery monitor code
    Signed-off-by: Marko Puric <marko@golioth.io>

--- src/app_work.c
+++ src/app_work.c
@@ -12,13 +12,6 @@ LOG_MODULE_REGISTER(app_work, LOG_LEVEL_DBG);
 
 #include "app_work.h"
 
-#ifdef CONFIG_LIB_OSTENTUS
-#include <libostentus.h>
-#endif
-#ifdef CONFIG_ALUDEL_BATTERY_MONITOR
-#include "battery_monitor/battery.h"
-#endif
-
 static struct golioth_client *client;
 /* Add Sensor structs here */
 
@@ -42,14 +35,6 @@ void app_work_sensor_read(void)
 	int err;
 	char json_buf[256];
 
-	IF_ENABLED(CONFIG_ALUDEL_BATTERY_MONITOR, (
-		read_and_report_battery();
-		IF_ENABLED(CONFIG_LIB_OSTENTUS, (
-			slide_set(BATTERY_V, get_batt_v_str(), strlen(get_batt_v_str()));
-			slide_set(BATTERY_LVL, get_batt_lvl_str(), strlen(get_batt_lvl_str()));
-		));
-	));
-
 	/* For this demo, we just send Hello to Golioth */
 	static uint8_t counter;
 
@@ -71,16 +56,6 @@ void app_work_sensor_read(void)
 		LOG_ERR("Failed to send sensor data to Golioth: %d", err);
 	}
 
-	IF_ENABLED(CONFIG_LIB_OSTENTUS, (
-		/* Update slide values on Ostentus
-		 *  -values should be sent as strings
-		 *  -use the enum from app_work.h for slide key values
-		 */
-		snprintk(json_buf, sizeof(json_buf), "%d", counter);
-		slide_set(UP_COUNTER, json_buf, strlen(json_buf));
-		snprintk(json_buf, sizeof(json_buf), "%d", 255 - counter);
-		slide_set(DN_COUNTER, json_buf, strlen(json_buf));
-	));
 	++counter;
 }
 
